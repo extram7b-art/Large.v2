ac7f910f2c9825276bc42168d05739095ce6b750
extram7b-art <extram7b@gmail.com>
2026-01-12 22:11:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-alert("JS WORKS");
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x00ff00);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(cube);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
